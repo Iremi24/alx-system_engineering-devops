edeabb78e8eadbe53975a153de7d5be1e6f53434
Adegboro Moradeyo <iremamae2018@gmail.com>
2024-05-04 17:23:35 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 #A file named school will be created
-file { '/tmp/school':'
-        ensure  =>  file,
-        content  =>  'I love Puppet',
-        mode  =>  '0744',
-        owner  =>  'www-data',
-        group  =>  'www-data',
+file { '/tmp/school':
+	mode	=> '0744',
+	owner	=> 'www-data',
+	group	=> 'www-data',
+	content	=> 'I love Puppet',
 }
