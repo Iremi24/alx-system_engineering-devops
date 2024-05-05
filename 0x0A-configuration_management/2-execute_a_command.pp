aa185673e3d3491baa8847d7b0c068fda534c426
Adegboro Moradeyo <iremamae2018@gmail.com>
2024-05-04 18:24:34 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,6 @@
-# Executes a command
-exec { 'pkill killmenow':
-	path => '/usr/bin:/usr/sbin:/bin'
+#This code kills a process && works together with the killmenow file which has already been provided
+exec { 'killmenow',:
+	command  => '/usr/bin/pkill killmenow',
+	provider => 'shell',
+	returns	 => [0, 1].
 }
