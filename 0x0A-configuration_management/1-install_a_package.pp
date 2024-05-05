c62e3102a3e423108ca231da31576c7532f9d242
Adegboro Moradeyo <iremamae2018@gmail.com>
2024-05-05 16:49:17 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# Install a version of flask(2.1.0)
-package {'flask':
-	ensure	 => '2.1.0',
-	provider => 'pip3'
+# Puppet manifest to install Flask package
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
